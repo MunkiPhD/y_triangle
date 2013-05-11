8c225cd2a4690a1ed85b38317150717f5d4f864f
MunkiPhD <munkiphd@gmail.com>
2013-05-11 00:35:56 -0400

    FULL CODE INCOMING! - initial commit

---
+++ YodleTriangle/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace YodleTriangle {
+    partial class YodleTriangle {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tboxFileLocation = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Assumptions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Each Row has n+1 elements";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Desired node in adjacent row can be expressed by:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(258, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " round( (elements - 1)  / 2) when elements > 2, else, 0";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalculate.Location = new System.Drawing.Point(369, 101);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(130, 37);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Sum";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // tboxFileLocation
+            // 
+            this.tboxFileLocation.Location = new System.Drawing.Point(12, 32);
+            this.tboxFileLocation.Name = "tboxFileLocation";
+            this.tboxFileLocation.Size = new System.Drawing.Size(445, 20);
+            this.tboxFileLocation.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "File Location:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(463, 29);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(37, 23);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt Files|*.txt|All Files|*>*";
+            this.openFileDialog1.Title = "Find the triangle.txt file";
+            // 
+            // YodleTriangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 150);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tboxFileLocation);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "YodleTriangle";
+            this.Text = "Yodle Triangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox tboxFileLocation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ YodleTriangle/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace YodleTriangle {
+    /// <summary>
+    /// Calculates the sum of the adjacent nodes in subsequent rows starting at the root node
+    /// </summary>
+    public partial class YodleTriangle : Form {
+        public YodleTriangle() {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e) {
+            try {
+                //get the file name from the textbox in case the user changed a path
+                string fileLocation = tboxFileLocation.Text.Trim();
+                //check that the file exists
+                if(File.Exists(fileLocation)) {
+                    int runningSum = CalculateSum(fileLocation);
+                    MessageBox.Show(String.Format("Sum: {0}", runningSum), "Running Sum", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else {
+                    MessageBox.Show("File does not exist");
+                }
+            } catch(Exception exc) {
+                MessageBox.Show(exc.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        /// <summary>
+        /// Calculates the sum of the adjacent nodes in subsequent rows of the tree starting from the root node
+        /// </summary>
+        /// <param name="fileLocation"></param>
+        private int CalculateSum(String fileLocation) {
+            // if an exception occurs, let it bubble up. Since we're checking for file existence in the calling method,
+            // going to skip checking for it here
+
+            //read the file line by line
+            int counter = 0;
+            string currentLine;
+            int runningSum = 0;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(fileLocation);
+            while((currentLine = file.ReadLine()) != null) {
+                String[] splitString = currentLine.Trim().Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+                int middle = 0;
+                int numerator = (splitString.Length - 1); // since using integer, it will round up if it's inbetween (e.g. 3.5)
+                
+                if(numerator > 1) { // check for greater than one so that we get the correct location for rows 0 and 1 (root and next row)
+                    middle = numerator / 2;
+                } else if(numerator == 1) { // for the case where there are 2 elements, we want the one in position 0
+                    middle = 0;
+                } else { // for the case where there is only one element, the root
+                    middle = 0;
+                }
+
+                runningSum += int.Parse(splitString[middle]);
+                counter++;
+            }
+
+            file.Close();
+
+            return runningSum;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e) {
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                tboxFileLocation.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
